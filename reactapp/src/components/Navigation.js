d2053e81b44f82d68aa65cba7151e00260ac10e8
HammondBrandon-FS <86680434+HammondBrandon-FS@users.noreply.github.com>
2023-03-23 11:55:23 -0500

    react backbone creation

---
+++ reactapp/src/components/Navigation.js
@@ -0,0 +1,11 @@
+// Navigation Component
+
+const Navigation = () => {
+    return (
+        <div>
+            <h2>Navigation</h2>
+        </div>
+    );
+}
+
+export default Navigation;
---
+++ reactapp/src/pages/Home.js
@@ -0,0 +1,11 @@
+// Home page
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Take a Quiz!</h2>
+        </div>
+    );
+}
+
+export default Home;
